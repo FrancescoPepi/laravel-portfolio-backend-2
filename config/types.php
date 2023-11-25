76ed64f78c2be46ca9d286e032b138d586f08aea
FrancescoPepi <francescopepi16@gmail.com>
2023-11-25 22:56:25 +0100

    ADD migrate one to many/array type

---
+++ config/types.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'types' => [
+        [
+            'label' => 'Front-End',
+            'color' => '',
+        ],
+        [
+            'label' => 'Back-End',
+            'color' => '',
+        ],
+        [
+            'label' => 'Full-Stack',
+            'color' => '',
+        ],
+    ]
+];
